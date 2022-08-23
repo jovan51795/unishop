a6bb36171f193d7b10f666446e70905e8a0afd9e
pmonisit <41044547+pmonisit@users.noreply.github.com>
2022-08-23 14:53:13 +0800

    Created checkout page

--- unishop/src/app/modules/user/checkout/checkout.component.ts
+++ unishop/src/app/modules/user/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/core/services/cart.service';
+import { CartItem } from 'src/app/models/cart';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  items: CartItem[] | undefined;
+  total: number | undefined;
+
+  constructor(private cartService: CartService, private router: Router,) { }
 
   ngOnInit(): void {
   }
+
+   placeOrder() {
+    alert("Order has been placed")
+    this.router.navigate(['/home']);
+  }
+
+   backToCart() {
+    this.router.navigate(['user/my-cart']);
+  }
+
 
 }
