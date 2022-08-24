ce460422e2705e2f3990b3226d3e1fe8a0f56b9d
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2022-08-24 15:56:48 +0800

    view product page added

--- unishop/src/app/components/card/card.component.ts
+++ unishop/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  @Input() data: any
 
-  constructor() { }
+  constructor() {
+    console.log(this.data, "from card")
+   }
 
   ngOnInit(): void {
   }
+
 
 }
--- unishop/src/app/modules/admin/details/details.component.ts
+++ unishop/src/app/modules/admin/details/details.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AdminService } from 'src/app/core/services/admin/admin.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent {
+  paramID: any;
+  value: any[] = []
+  constructor(private route: ActivatedRoute, private adminServices: AdminService) { 
+    this.route.queryParams.subscribe(data => {
+      this.paramID = data['id']
+      
+    })
+    this.adminServices.getProductById(parseInt(this.paramID)).subscribe(data => {
+      this.modifyData(data[0])
+    })
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
+
+  modifyData (data: any) {
+    let prop = Object.keys(data)
+    prop.forEach(x => {
+      console.log(x)
+      if(x !== 'id'){
+        if(data[x] instanceof Object && data[x].length !== 0){
+          this.value.push({name: x,key: data[x], hasChild: true})
+        }
+        else {
+          this.value.push({name: x,key: data[x], hasChild: false})
+        }
+      }
+      
+    })
+  }
+
 
 }
