2f1f7dac9fd3fca53a06efd23f34f53f8f223506
JohnArima <john.salangsang20@gmail.com>
2022-08-30 14:14:16 +0800

    update on user various files

--- unishop/src/app/pages/home/home.component.ts
+++ unishop/src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CartService } from 'src/app/core/services/cart/cart.service';
+import { ProductsService } from 'src/app/core/services/product/products.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public productList: any[] | undefined;
+  public filterCategory : any;
+  searchKey: string = "";
+  public searchTerm !: string;
 
-  constructor() { }
+  constructor(
+    private productService: ProductsService, 
+    private cartService: CartService, 
+    private router: Router,
+    private toast: ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.productService.getProduct().subscribe(res => {
+      this.productList = res;
+      this.filterCategory = res;
+
+      this.productList.forEach((a : any) => {
+        Object.assign(a, {quantity : 1, total : a.price})
+      });
+    });
+
+    this.cartService.search.subscribe((val: any) => {
+      this.searchKey = val;
+    })
   }
 
+  addToCart(item : any){
+    this.toast.success("Added to cart")
+    this.cartService.addToCart(item);
+  }
+  
+  goToCart(){
+    this.router.navigate(['user/my-cart']);
+  }
+
+  search(event:any){
+    this.searchTerm = (event.target as HTMLInputElement).value;
+    console.log(this.searchTerm);
+    this.cartService.search.next(this.searchTerm);
+  }
+
+  filter(category: string){
+    this.filterCategory = this.productList!
+    .filter((a:any) => {
+      if(a.category === category || category === ''){
+          return a;
+      }
+    })
+  }
 }
