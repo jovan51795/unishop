ddf87bac7e5a3c64a84c31b0d3b608205317016b
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2023-05-31 15:47:59 +0800

    sample commit

--- unishop/src/app/shared/pages-layout/pages-layout.component.ts
+++ unishop/src/app/shared/pages-layout/pages-layout.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pages-layout',
   templateUrl: './pages-layout.component.html',
-  styleUrls: ['./pages-layout.component.scss']
+  styleUrls: ['./pages-layout.component.scss'],
 })
 export class PagesLayoutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
